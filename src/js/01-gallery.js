5f1ce99472396d917e9564a82a0d688831d56984
Dmitry Yurov <yurov37@gmail.com>
2022-09-20 13:36:36 +0300

    task 1 & 3 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const imageGallery = document.querySelector('.gallery');
+const imagesMarkup = createImageCardsMarkup(galleryItems);
+
+imageGallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImageCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const feedbackFormData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  feedbackFormData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
+}
+
+function onFormSubmit(evt) {
+  //   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function savedFeedbackData() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+}
+savedFeedbackData();
